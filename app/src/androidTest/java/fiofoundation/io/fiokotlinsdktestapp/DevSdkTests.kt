b4e820dac7e85f1e707ce21d9569a881f4a27d3a
Shawn Mullen <shawn@shawnmullen.com>
2020-01-07 20:32:36 +0900

    Dev unit test set up changes.

--- app/src/androidTest/java/fiofoundation/io/fiokotlinsdktestapp/DevSdkTests.kt
+++ app/src/androidTest/java/fiofoundation/io/fiokotlinsdktestapp/DevSdkTests.kt
@@ -38,7 +38,7 @@ class DevSdkTests
     private var bobFioAddress = ""
 
     private var fioTestDomain = "brd"
-    private var defaultFee = BigInteger("300000000000")
+    private var defaultFee = BigInteger("500000000000")
 
     private val alicePublicTokenAddress = "1PzCN3cBkTL72GPeJmpcueU4wQi9guiLa6"
     private val alicePublicTokenCode = "BTC"
@@ -49,6 +49,9 @@ class DevSdkTests
     private var bobFioSdk:FIOSDK? = null
 
     private var logTag = "FIOSDK-TEST"
+
+    private var useMockServerForAlice = false
+    private var useMockServerForBob = false
 
     @Test
     @ExperimentalUnsignedTypes
@@ -813,10 +816,18 @@ class DevSdkTests
         this.bobFioSdk = createSdkInstance(this.bobPrivateKey,this.bobPublicKey)
 
         val initialFioAddressForAlice = this.generateTestingFioAddress()
-        this.registerFioNameForUser(this.aliceFioSdk!!,initialFioAddressForAlice)
+
+        if(useMockServerForAlice)
+            this.registerFioNameForUser(this.aliceFioSdk!!,initialFioAddressForAlice)
+        else
+            this.aliceFioSdk!!.registerFioAddress(initialFioAddressForAlice,this.defaultFee)
 
         val initialFioAddressForBob = this.generateTestingFioAddress()
-        this.registerFioNameForUser(this.bobFioSdk!!,initialFioAddressForBob)
+
+        if(useMockServerForBob)
+            this.registerFioNameForUser(this.bobFioSdk!!,initialFioAddressForBob)
+        else
+            this.bobFioSdk!!.registerFioAddress(initialFioAddressForBob,this.defaultFee)
 
         this.aliceFioAddress = initialFioAddressForAlice
         this.bobFioAddress = initialFioAddressForBob
@@ -824,16 +835,20 @@ class DevSdkTests
         println("Alice's Public Key: " + this.alicePublicKey)
         println("Bob's Public Key: " + this.bobPublicKey)
 
-        this.requestFaucetFunds("25")
-        Thread.sleep(4000)
+        if(useMockServerForAlice || useMockServerForBob) {
+            this.requestFaucetFunds("25")
+            Thread.sleep(4000)
 
-        this.requestFaucetFunds("25")
-        Thread.sleep(4000)
+            this.requestFaucetFunds("25")
+            Thread.sleep(4000)
 
-        this.requestFaucetFunds("25")
+            this.requestFaucetFunds("25")
 
-        Log.i(this.logTag, "Wait for balance to really be available.")
-        Thread.sleep(60000)
+            Log.i(this.logTag, "Wait for balance to really be available.")
+            Thread.sleep(60000)
+        }
+
+
     }
 
     fun registerFioNameForUser(fioSdk:FIOSDK,fioAddress:String) {
@@ -884,12 +899,20 @@ class DevSdkTests
 
         var mn = getRandomSeedWords().joinToString(" ")
 
-        alicePrivateKey = FIOSDK.createPrivateKey(mn)
+        if(this.alicePrivateKey == "") {
+            alicePrivateKey = FIOSDK.createPrivateKey(mn)
+            useMockServerForAlice = true
+        }
+
         alicePublicKey = FIOSDK.derivedPublicKey(alicePrivateKey)
 
         mn = getRandomSeedWords().joinToString(" ")
 
-        bobPrivateKey = FIOSDK.createPrivateKey(mn)
+        if(this.bobPrivateKey == "") {
+            bobPrivateKey = FIOSDK.createPrivateKey(mn)
+            useMockServerForBob = true
+        }
+
         bobPublicKey = FIOSDK.derivedPublicKey(bobPrivateKey)
     }
 
